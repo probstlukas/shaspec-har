88b04ca9b649984c7b502ce78f3b168fa29128ee
Lukas Probst <lukas.probst@student.kit.edu>
2024-03-10 15:13:06 +0100

    Change training template

--- template.sh
+++ template.sh
@@ -1,54 +1,54 @@
-sbatch main.py --data-name dsads --miss-rate 0.0 --shuffle --model-type shaspec --seed 1 --root-path ../../../datasets
-sbatch main.py --data-name dsads --miss-rate 0.2 --shuffle --model-type shaspec --seed 1 --root-path ../../../datasets
-sbatch main.py --data-name dsads --miss-rate 0.4 --shuffle --model-type shaspec --seed 1 --root-path ../../../datasets
-sbatch main.py --data-name dsads --miss-rate 0.6 --shuffle --model-type shaspec --seed 1 --root-path ../../../datasets
-sbatch main.py --data-name realdisp --miss-rate 0.0 --shuffle --model-type shaspec --seed 1 --root-path ../../../datasets
-sbatch main.py --data-name realdisp --miss-rate 0.1 --shuffle --model-type shaspec --seed 1 --root-path ../../../datasets
-sbatch main.py --data-name realdisp --miss-rate 0.3 --shuffle --model-type shaspec --seed 1 --root-path ../../../datasets
-sbatch main.py --data-name realdisp --miss-rate 0.5 --shuffle --model-type shaspec --seed 1 --root-path ../../../datasets
-sbatch main.py --data-name realdisp --miss-rate 0.7 --shuffle --model-type shaspec --seed 1 --root-path ../../../datasets
-sbatch main.py --data-name dsads --miss-rate 0.0 --use-shared-encoder False --shuffle --model-type shaspec --seed 1 --root-path ../../../datasets
-sbatch main.py --data-name dsads --miss-rate 0.2 --use-shared-encoder False --shuffle --model-type shaspec --seed 1 --root-path ../../../datasets
-sbatch main.py --data-name dsads --miss-rate 0.4 --use-shared-encoder False --shuffle --model-type shaspec --seed 1 --root-path ../../../datasets
-sbatch main.py --data-name dsads --miss-rate 0.6 --use-shared-encoder False --shuffle --model-type shaspec --seed 1 --root-path ../../../datasets
-sbatch main.py --data-name realdisp --miss-rate 0.0 --use-shared-encoder False --shuffle --model-type shaspec --seed 1 --root-path ../../../datasets
-sbatch main.py --data-name realdisp --miss-rate 0.1 --use-shared-encoder False --shuffle --model-type shaspec --seed 1 --root-path ../../../datasets
-sbatch main.py --data-name realdisp --miss-rate 0.3 --use-shared-encoder False --shuffle --model-type shaspec --seed 1 --root-path ../../../datasets
-sbatch main.py --data-name realdisp --miss-rate 0.5 --use-shared-encoder False --shuffle --model-type shaspec --seed 1 --root-path ../../../datasets
-sbatch main.py --data-name realdisp --miss-rate 0.7 --use-shared-encoder False --shuffle --model-type shaspec --seed 1 --root-path ../../../datasets
-sbatch main.py --data-name dsads --miss-rate 0.0 --use-missing-modality-features False --shuffle --model-type shaspec --seed 1 --root-path ../../../datasets
-sbatch main.py --data-name dsads --miss-rate 0.2 --use-missing-modality-features False --shuffle --model-type shaspec --seed 1 --root-path ../../../datasets
-sbatch main.py --data-name dsads --miss-rate 0.4 --use-missing-modality-features False --shuffle --model-type shaspec --seed 1 --root-path ../../../datasets
-sbatch main.py --data-name dsads --miss-rate 0.6 --use-missing-modality-features False --shuffle --model-type shaspec --seed 1 --root-path ../../../datasets
-sbatch main.py --data-name realdisp --miss-rate 0.0 --use-missing-modality-features False --shuffle --model-type shaspec --seed 1 --root-path ../../../datasets
-sbatch main.py --data-name realdisp --miss-rate 0.1 --use-missing-modality-features False --shuffle --model-type shaspec --seed 1 --root-path ../../../datasets
-sbatch main.py --data-name realdisp --miss-rate 0.3 --use-missing-modality-features False --shuffle --model-type shaspec --seed 1 --root-path ../../../datasets
-sbatch main.py --data-name realdisp --miss-rate 0.5 --use-missing-modality-features False --shuffle --model-type shaspec --seed 1 --root-path ../../../datasets
-sbatch main.py --data-name realdisp --miss-rate 0.7 --use-missing-modality-features False --shuffle --model-type shaspec --seed 1 --root-path ../../../datasets
-sbatch main.py --data-name dsads --miss-rate 0.0 --shuffle --model-type shaspec --seed 42 --root-path ../../../datasets
-sbatch main.py --data-name dsads --miss-rate 0.2 --shuffle --model-type shaspec --seed 42 --root-path ../../../datasets
-sbatch main.py --data-name dsads --miss-rate 0.4 --shuffle --model-type shaspec --seed 42 --root-path ../../../datasets
-sbatch main.py --data-name dsads --miss-rate 0.6 --shuffle --model-type shaspec --seed 42 --root-path ../../../datasets
-sbatch main.py --data-name realdisp --miss-rate 0.0 --shuffle --model-type shaspec --seed 42 --root-path ../../../datasets
-sbatch main.py --data-name realdisp --miss-rate 0.1 --shuffle --model-type shaspec --seed 42 --root-path ../../../datasets
-sbatch main.py --data-name realdisp --miss-rate 0.3 --shuffle --model-type shaspec --seed 42 --root-path ../../../datasets
-sbatch main.py --data-name realdisp --miss-rate 0.5 --shuffle --model-type shaspec --seed 42 --root-path ../../../datasets
-sbatch main.py --data-name realdisp --miss-rate 0.7 --shuffle --model-type shaspec --seed 42 --root-path ../../../datasets
-sbatch main.py --data-name dsads --miss-rate 0.0 --use-shared-encoder False --shuffle --model-type shaspec --seed 42 --root-path ../../../datasets
-sbatch main.py --data-name dsads --miss-rate 0.2 --use-shared-encoder False --shuffle --model-type shaspec --seed 42 --root-path ../../../datasets
-sbatch main.py --data-name dsads --miss-rate 0.4 --use-shared-encoder False --shuffle --model-type shaspec --seed 42 --root-path ../../../datasets
-sbatch main.py --data-name dsads --miss-rate 0.6 --use-shared-encoder False --shuffle --model-type shaspec --seed 42 --root-path ../../../datasets
-sbatch main.py --data-name realdisp --miss-rate 0.0 --use-shared-encoder False --shuffle --model-type shaspec --seed 42 --root-path ../../../datasets
-sbatch main.py --data-name realdisp --miss-rate 0.1 --use-shared-encoder False --shuffle --model-type shaspec --seed 42 --root-path ../../../datasets
-sbatch main.py --data-name realdisp --miss-rate 0.3 --use-shared-encoder False --shuffle --model-type shaspec --seed 42 --root-path ../../../datasets
-sbatch main.py --data-name realdisp --miss-rate 0.5 --use-shared-encoder False --shuffle --model-type shaspec --seed 42 --root-path ../../../datasets
-sbatch main.py --data-name realdisp --miss-rate 0.7 --use-shared-encoder False --shuffle --model-type shaspec --seed 42 --root-path ../../../datasets
-sbatch main.py --data-name dsads --miss-rate 0.0 --use-missing-modality-features False --shuffle --model-type shaspec --seed 42 --root-path ../../../datasets
-sbatch main.py --data-name dsads --miss-rate 0.2 --use-missing-modality-features False --shuffle --model-type shaspec --seed 42 --root-path ../../../datasets
-sbatch main.py --data-name dsads --miss-rate 0.4 --use-missing-modality-features False --shuffle --model-type shaspec --seed 42 --root-path ../../../datasets
-sbatch main.py --data-name dsads --miss-rate 0.6 --use-missing-modality-features False --shuffle --model-type shaspec --seed 42 --root-path ../../../datasets
-sbatch main.py --data-name realdisp --miss-rate 0.0 --use-missing-modality-features False --shuffle --model-type shaspec --seed 42 --root-path ../../../datasets
-sbatch main.py --data-name realdisp --miss-rate 0.1 --use-missing-modality-features False --shuffle --model-type shaspec --seed 42 --root-path ../../../datasets
-sbatch main.py --data-name realdisp --miss-rate 0.3 --use-missing-modality-features False --shuffle --model-type shaspec --seed 42 --root-path ../../../datasets
-sbatch main.py --data-name realdisp --miss-rate 0.5 --use-missing-modality-features False --shuffle --model-type shaspec --seed 42 --root-path ../../../datasets
-sbatch main.py --data-name realdisp --miss-rate 0.7 --use-missing-modality-features False --shuffle --model-type shaspec --seed 42 --root-path ../../../datasets
+sbatch main.py --data-name dsads --miss-rate 0.0 --shuffle --model-type shaspec --seed 1 --root-path ../datasets
+sbatch main.py --data-name dsads --miss-rate 0.2 --shuffle --model-type shaspec --seed 1 --root-path ../datasets
+sbatch main.py --data-name dsads --miss-rate 0.4 --shuffle --model-type shaspec --seed 1 --root-path ../datasets
+sbatch main.py --data-name dsads --miss-rate 0.6 --shuffle --model-type shaspec --seed 1 --root-path ../datasets
+sbatch main.py --data-name realdisp --miss-rate 0.0 --shuffle --model-type shaspec --seed 1 --root-path ../datasets
+sbatch main.py --data-name realdisp --miss-rate 0.1 --shuffle --model-type shaspec --seed 1 --root-path ../datasets
+sbatch main.py --data-name realdisp --miss-rate 0.3 --shuffle --model-type shaspec --seed 1 --root-path ../datasets
+sbatch main.py --data-name realdisp --miss-rate 0.5 --shuffle --model-type shaspec --seed 1 --root-path ../datasets
+sbatch main.py --data-name realdisp --miss-rate 0.7 --shuffle --model-type shaspec --seed 1 --root-path ../datasets
+sbatch main.py --data-name dsads --miss-rate 0.0 --use-shared-encoder False --shuffle --model-type shaspec --seed 1 --root-path ../datasets
+sbatch main.py --data-name dsads --miss-rate 0.2 --use-shared-encoder False --shuffle --model-type shaspec --seed 1 --root-path ../datasets
+sbatch main.py --data-name dsads --miss-rate 0.4 --use-shared-encoder False --shuffle --model-type shaspec --seed 1 --root-path ../datasets
+sbatch main.py --data-name dsads --miss-rate 0.6 --use-shared-encoder False --shuffle --model-type shaspec --seed 1 --root-path ../datasets
+sbatch main.py --data-name realdisp --miss-rate 0.0 --use-shared-encoder False --shuffle --model-type shaspec --seed 1 --root-path ../datasets
+sbatch main.py --data-name realdisp --miss-rate 0.1 --use-shared-encoder False --shuffle --model-type shaspec --seed 1 --root-path ../datasets
+sbatch main.py --data-name realdisp --miss-rate 0.3 --use-shared-encoder False --shuffle --model-type shaspec --seed 1 --root-path ../datasets
+sbatch main.py --data-name realdisp --miss-rate 0.5 --use-shared-encoder False --shuffle --model-type shaspec --seed 1 --root-path ../datasets
+sbatch main.py --data-name realdisp --miss-rate 0.7 --use-shared-encoder False --shuffle --model-type shaspec --seed 1 --root-path ../datasets
+sbatch main.py --data-name dsads --miss-rate 0.0 --use-missing-modality-features False --shuffle --model-type shaspec --seed 1 --root-path ../datasets
+sbatch main.py --data-name dsads --miss-rate 0.2 --use-missing-modality-features False --shuffle --model-type shaspec --seed 1 --root-path ../datasets
+sbatch main.py --data-name dsads --miss-rate 0.4 --use-missing-modality-features False --shuffle --model-type shaspec --seed 1 --root-path ../datasets
+sbatch main.py --data-name dsads --miss-rate 0.6 --use-missing-modality-features False --shuffle --model-type shaspec --seed 1 --root-path ../datasets
+sbatch main.py --data-name realdisp --miss-rate 0.0 --use-missing-modality-features False --shuffle --model-type shaspec --seed 1 --root-path ../datasets
+sbatch main.py --data-name realdisp --miss-rate 0.1 --use-missing-modality-features False --shuffle --model-type shaspec --seed 1 --root-path ../datasets
+sbatch main.py --data-name realdisp --miss-rate 0.3 --use-missing-modality-features False --shuffle --model-type shaspec --seed 1 --root-path ../datasets
+sbatch main.py --data-name realdisp --miss-rate 0.5 --use-missing-modality-features False --shuffle --model-type shaspec --seed 1 --root-path ../datasets
+sbatch main.py --data-name realdisp --miss-rate 0.7 --use-missing-modality-features False --shuffle --model-type shaspec --seed 1 --root-path ../datasets
+sbatch main.py --data-name dsads --miss-rate 0.0 --shuffle --model-type shaspec --seed 42 --root-path ../datasets
+sbatch main.py --data-name dsads --miss-rate 0.2 --shuffle --model-type shaspec --seed 42 --root-path ../datasets
+sbatch main.py --data-name dsads --miss-rate 0.4 --shuffle --model-type shaspec --seed 42 --root-path ../datasets
+sbatch main.py --data-name dsads --miss-rate 0.6 --shuffle --model-type shaspec --seed 42 --root-path ../datasets
+sbatch main.py --data-name realdisp --miss-rate 0.0 --shuffle --model-type shaspec --seed 42 --root-path ../datasets
+sbatch main.py --data-name realdisp --miss-rate 0.1 --shuffle --model-type shaspec --seed 42 --root-path ../datasets
+sbatch main.py --data-name realdisp --miss-rate 0.3 --shuffle --model-type shaspec --seed 42 --root-path ../datasets
+sbatch main.py --data-name realdisp --miss-rate 0.5 --shuffle --model-type shaspec --seed 42 --root-path ../datasets
+sbatch main.py --data-name realdisp --miss-rate 0.7 --shuffle --model-type shaspec --seed 42 --root-path ../datasets
+sbatch main.py --data-name dsads --miss-rate 0.0 --use-shared-encoder False --shuffle --model-type shaspec --seed 42 --root-path ../datasets
+sbatch main.py --data-name dsads --miss-rate 0.2 --use-shared-encoder False --shuffle --model-type shaspec --seed 42 --root-path ../datasets
+sbatch main.py --data-name dsads --miss-rate 0.4 --use-shared-encoder False --shuffle --model-type shaspec --seed 42 --root-path ../datasets
+sbatch main.py --data-name dsads --miss-rate 0.6 --use-shared-encoder False --shuffle --model-type shaspec --seed 42 --root-path ../datasets
+sbatch main.py --data-name realdisp --miss-rate 0.0 --use-shared-encoder False --shuffle --model-type shaspec --seed 42 --root-path ../datasets
+sbatch main.py --data-name realdisp --miss-rate 0.1 --use-shared-encoder False --shuffle --model-type shaspec --seed 42 --root-path ../datasets
+sbatch main.py --data-name realdisp --miss-rate 0.3 --use-shared-encoder False --shuffle --model-type shaspec --seed 42 --root-path ../datasets
+sbatch main.py --data-name realdisp --miss-rate 0.5 --use-shared-encoder False --shuffle --model-type shaspec --seed 42 --root-path ../datasets
+sbatch main.py --data-name realdisp --miss-rate 0.7 --use-shared-encoder False --shuffle --model-type shaspec --seed 42 --root-path ../datasets
+sbatch main.py --data-name dsads --miss-rate 0.0 --use-missing-modality-features False --shuffle --model-type shaspec --seed 42 --root-path ../datasets
+sbatch main.py --data-name dsads --miss-rate 0.2 --use-missing-modality-features False --shuffle --model-type shaspec --seed 42 --root-path ../datasets
+sbatch main.py --data-name dsads --miss-rate 0.4 --use-missing-modality-features False --shuffle --model-type shaspec --seed 42 --root-path ../datasets
+sbatch main.py --data-name dsads --miss-rate 0.6 --use-missing-modality-features False --shuffle --model-type shaspec --seed 42 --root-path ../datasets
+sbatch main.py --data-name realdisp --miss-rate 0.0 --use-missing-modality-features False --shuffle --model-type shaspec --seed 42 --root-path ../datasets
+sbatch main.py --data-name realdisp --miss-rate 0.1 --use-missing-modality-features False --shuffle --model-type shaspec --seed 42 --root-path ../datasets
+sbatch main.py --data-name realdisp --miss-rate 0.3 --use-missing-modality-features False --shuffle --model-type shaspec --seed 42 --root-path ../datasets
+sbatch main.py --data-name realdisp --miss-rate 0.5 --use-missing-modality-features False --shuffle --model-type shaspec --seed 42 --root-path ../datasets
+sbatch main.py --data-name realdisp --miss-rate 0.7 --use-missing-modality-features False --shuffle --model-type shaspec --seed 42 --root-path ../datasets
